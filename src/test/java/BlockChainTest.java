9c27e1489cba72ec423694abbc81e60f73feae56
unknown <patezno97@gmail.com>
2019-02-13 20:00:40 +0100

    created some testcases

---
+++ src/test/java/BlockChainTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class BlockChainTest {
+
+    private BlockChain blockChain = null;
+
+    @Before
+    public void init() {
+        blockChain = new BlockChain();
+    }
+
+    @Test
+    public void createBlockChainTest() {
+        assertNotNull(blockChain);
+    }
+
+    @Test
+    public void isBlockChainCreatedTest() {
+        assertNotNull(blockChain.getBlockChain());
+    }
+}
